d1fb3437c617297820fc99d490007f75656d49d3
monaco89 <nick.monaco15@gmail.com>
2020-08-29 14:44:56 -0400

    init

---
+++ src/constants/config.js
@@ -0,0 +1,5 @@
+export const config = {
+  env: {
+    serverUri: process.env.REACT_APP_SERVER_URI || 'http://localhost:8000',
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider,
+  ApolloLink,
+} from '@apollo/client';
+import { onError } from '@apollo/link-error';
+import { RetryLink } from '@apollo/link-retry';
+import { config } from './constants/config';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext(({ headers = {} }) => ({
+    headers: {
+      ...headers,
+    },
+  }));
+
+  return forward(operation);
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message }) => {
+      // TODO Log errors
+      console.log('Error', message);
+    });
+  }
+  if (networkError) {
+    // TODO Log errors
+    console.log('Network error', networkError);
+  }
+});
+
+const link = ApolloLink.from([
+  authLink,
+  new RetryLink(),
+  errorLink,
+  new HttpLink({
+    uri: config.env.serverUri,
+  }),
+]);
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link,
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
